1ee1440b5ad8fde08a41707480635d5bd069169c
Luc Kadletz <luc.kadletz@digipen.edu>
2017-01-20 14:36:45 -0800

    Added Input System & Test Controller
    Auston is a bro

---
+++ Arena/Assets/Arena/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public int PlayerNum;
+    public float Speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        InputEvents.Movement.Subscribe(OnMovement, PlayerNum);
+	}
+
+    void OnMovement(InputEventInfo info)
+    {
+        Vector3 worldMovement = new Vector3(info.dualAxisValue.x, 0, info.dualAxisValue.y);
+        transform.position += worldMovement * Speed * Time.deltaTime;
+    }
+}
