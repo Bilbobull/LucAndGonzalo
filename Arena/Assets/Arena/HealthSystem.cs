e1ad32203f262d05e483bd0cd25d57058e3d4c0d
Bilbobull <gonzalo.r@digipen.edu>
2017-01-22 20:44:28 -0800

    Health System

---
+++ Arena/Assets/Arena/HealthSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour {
+
+    public int MaxHealth = 5;
+    public int Damage = 1;
+    private int Health = 0;
+
+	// Use this for initialization
+	void Start () {
+        Health = MaxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionStart(Collision hit)
+    {
+        if ((this.name == "Enemy" && hit.gameObject.tag == "Player") || (this.tag == "Player" && hit.gameObject.name == "Enemy"))
+        {
+            Health -= Damage;
+            if (Health <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+}
