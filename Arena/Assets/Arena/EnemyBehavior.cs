6f81403b95d2865ff3fabe4314e7d53eab2f581c
Bilbobull <gonzalo.r@digipen.edu>
2017-01-27 20:37:48 -0800

    Movements
    Towards Player
    Away
    Wander
    ZigZag

--- Arena/Assets/Arena/EnemyBehavior.cs
+++ Arena/Assets/Arena/EnemyBehavior.cs
@@ -1,37 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyBehavior : MonoBehaviour {
+public class EnemyBehavior : MonoBehaviour
+{
     public float speed = 1.0f;
     public string playertag = "Player";
+    public int zigzagradius = 2;
     private GameObject[] players;
     private GameObject ClosestPlayer;
     private float timer = 0;
     private int MovementType = 0;
+    private Vector3 Direction;
+    private float PassedTime = 0;
+     
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (timer <= 0.0f)
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer <= 0.0f || !ClosestPlayer)
         {
             players = GameObject.FindGameObjectsWithTag(playertag);
             ClosestPlayer = FindClosestPlayer();
-            MovementType = Random.Range(1, 1);
-
-            switch (MovementType)
-            {
-                case 1:
-                    timer = Random.Range(1.0f, 3.0f);
-                    break;
-
-
-                default:
-                    break;
-            }
+            MovementType = Random.Range(1, 4);
+            timer = Random.Range(1.0f, 3.0f);
+            PassedTime = 0.0f;
         }
 
         switch (MovementType)
@@ -40,19 +38,33 @@ public class EnemyBehavior : MonoBehaviour {
                 WalkTowardsClosestPlayer();
                 break;
 
+            case 2:
+                WalkAwayFromClosestPlayer();
+                break;
+
+            case 3:
+                Wander();
+                break;
+
+            case 4:
+                ZigZag();
+                break;
 
             default:
                 break;
         }
 
+        Direction.Normalize();
+        this.transform.position += Direction * speed;
+
         timer -= Time.deltaTime;
-	}
+    }
 
     private GameObject FindClosestPlayer()
     {
         GameObject closest = null;
         float smallestDist = float.PositiveInfinity;
-        foreach(GameObject p in players)
+        foreach (GameObject p in players)
         {
             if (Vector3.Distance(p.transform.position, this.transform.position) < smallestDist)
                 closest = p;
@@ -63,9 +75,28 @@ public class EnemyBehavior : MonoBehaviour {
 
     private void WalkTowardsClosestPlayer()
     {
-        Vector3 direction = ClosestPlayer.transform.position - this.transform.position;
-        direction.Normalize();
-        this.transform.position += direction * speed;
+        Direction = ClosestPlayer.transform.position - this.transform.position;
+    }
+
+    private void WalkAwayFromClosestPlayer()
+    {
+        Direction = ClosestPlayer.transform.position - this.transform.position;
+    }
+
+    private void Wander()
+    {
+        Direction = new Vector3(Random.value, 0, Random.value);
+    }
+
+    private void ZigZag()
+    {
+        PassedTime += Time.deltaTime;   
+
+        Direction = ClosestPlayer.transform.position - this.transform.position;
+
+        Vector3 RightVec = new Vector3(-Direction.z, 0, Direction.x) * (zigzagradius* Mathf.Cos(PassedTime));
+
+        Direction += RightVec;
     }
 
 }
