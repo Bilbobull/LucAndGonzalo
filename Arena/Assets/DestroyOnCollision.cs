44bfad732d1ca51c0708de84a6e682bef3cc094b
Luc Kadletz <luc.kadletz@digipen.edu>
2017-03-27 12:49:07 -0700

    Bullets die when hitting level geometry
    Added destroyOnCollision component that lets you specify layers/tags to
    destroy when hitting

---
+++ Arena/Assets/DestroyOnCollision.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnCollision : MonoBehaviour {
+
+    public string[] tags;
+    public LayerMask layers;
+
+
+    public void OnCollisionEnter(Collision c)
+    {
+        if (IsDestroytarget(c.gameObject))
+            Destroy(gameObject);          
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (IsDestroytarget(other.gameObject))
+        {
+            Debug.Log("Destroy");
+            Destroy(gameObject);
+        }
+        else
+            Debug.Log("no destroy" + other.gameObject);
+
+    }
+
+    private static bool InMask(LayerMask mask, int layer)
+    {
+        return mask == (mask | 1 << layer);
+    }
+
+    // Returns true if colliding/triggering other object should destroy this one
+    bool IsDestroytarget(GameObject other)
+    {
+        // Destroy if in layer
+        if (InMask(layers, other.layer))
+        {
+            return true;
+        }
+        // Destroy if matches tag
+        foreach (string t in tags)
+            if (other.CompareTag(t))
+                return true;
+        // Otherwise don't destroy
+        return false;
+    }
+}
