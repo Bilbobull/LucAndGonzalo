1d06288a74dea6d4577031e61820b0bbcc267ec6
Bilbobull <gonzalo.r@digipen.edu>
2017-03-31 11:13:45 -0700

    Score system

--- Arena/Assets/Arena/HealthSystem.cs
+++ Arena/Assets/Arena/HealthSystem.cs
@@ -1,22 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthSystem : MonoBehaviour {
 
     public int MaxHealth = 5;
     private int Health = 0;
+    private ScoreTracker score;
 
 	// Use this for initialization
 	void Start () {
         Health = MaxHealth;
-	}
+        score = GameObject.Find("Canvas/Score_UI").GetComponent<ScoreTracker>();
+    }
 	
     public void SubstractHealth(int hp)
     {
         Health -= hp;
         if (Health <= 0)
+        {
+            if (this.tag == "Enemy")
+                score.score += 5;
+
             Destroy(this.gameObject);
+        }
     }
 
     public int GetHealth()
