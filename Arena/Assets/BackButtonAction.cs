f29f3e7361bd1df478ac1800919619ebe36b47fc
Luc Kadletz <luc.kadletz@digipen.edu>
2017-04-22 00:04:43 -0700

    I think we're done :rocket:

---
+++ Arena/Assets/BackButtonAction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackButtonAction : MonoBehaviour {
+
+    public string message;
+
+	// Use this for initialization
+	void Start () {
+        InputEvents.MenuBack.Subscribe(OnBackButton);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnBackButton(InputEventInfo info)
+    {
+        gameObject.SendMessage(message, SendMessageOptions.DontRequireReceiver);
+        Debug.Log(message);
+    }
+
+    void OnDestroy()
+    {
+        InputEvents.MenuBack.Unsubscribe(OnBackButton);
+    }
+}
---
+++ Arena/Assets/StartButtonAction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButtonAction : MonoBehaviour {
+
+    public string message;
+
+    // Use this for initialization
+    void Start()
+    {
+        InputEvents.MenuConfirm.Subscribe(OnStartButton);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnStartButton(InputEventInfo info)
+    {
+        gameObject.SendMessage(message, SendMessageOptions.DontRequireReceiver);
+    }
+
+    void OnDestroy()
+    {
+        InputEvents.MenuConfirm.Unsubscribe(OnStartButton);
+    }
+}
